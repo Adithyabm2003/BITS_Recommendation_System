b5b84c90d07f8f027df2fcf3ef11269f44957588
Adithya B M <103888807+Adithyabm2003@users.noreply.github.com>
2023-11-18 11:08:17 +0530

    Update RS_streamlite_app.py

--- RS_streamlite_app.py
+++ RS_streamlite_app.py
@@ -36,7 +36,7 @@ def recommend(movie):
     return recommended_movie_names,recommended_movie_posters
 
 
-st.header('Pragyan AI: Movie Recommender System')
+st.header('Adithya B M 's movie recommendation')
 movies = pickle.load(open('movie_list_1.pkl','rb'))
 similarity = pickle.load(open('similarity.pkl','rb'))
 
